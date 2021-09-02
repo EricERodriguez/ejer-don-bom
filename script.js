da73e6e461bc86fb3d3b90b93bcb96c18dffd9e8
Eric R <eric92rodriguez@gmail.com>
2021-09-02 18:25:00 -0300

    script2

--- script.js
+++ script.js
@@ -27,3 +27,51 @@ function adivinar() {
         }
     }
 }
+
+// generacion
+
+class Persona{
+    constructor(nombre,edad,dni,peso,altura){
+        this.nombre = nombre;
+        this.edad = edad;
+        this.dni = dni;
+        this.peso = peso;
+        this.altura = altura;
+    }
+
+        //setter & getter
+    set sex(sexo) {
+        if(sexo != "M" && sexo != "F") throw new Error('El valor no es permitido');
+        this.sexo = sexo;
+    }
+    get sex() {
+        return this.sexo;
+    }
+
+    mostrarGeneraciones(){
+        if ((new Date().getFullYear() - this.edad) < 1948 ) {
+            console.log("silent generation")
+        }else if ((new Date().getFullYear() - this.edad) < 1968 ) {
+            console.log("Baby Boom")
+        }else if ((new Date().getFullYear() - this.edad) < 1980 ) {
+            console.log("Genration X")
+        }else if ((new Date().getFullYear() - this.edad) < 1993 ) {
+            console.log("Genration Y")
+        }else{
+            console.log("Genration Z")
+        }
+
+    }
+    esMayorDeEdad(){
+        if (this.edad > 18) {
+            console.log("es mayor de edad")
+        }
+    }
+    mostrarDatos(){
+        console.log(`${this.nombre} ${this.edad} ${this.dni} ${this.sexo} ${this.peso} ${this.altura} `)
+    }
+
+    }
+
+    let personaNueva = new Persona("eric", 29, 36700700, 90, 171)
+    personaNueva.sex = "M";
